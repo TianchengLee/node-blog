5ae8fa7fb6fa5a5a28d3351b67f14ec60c5cf53c
TianchengLee <ltc6634284@gmail.com>
2018-11-06 17:07:24 +0800

    发表文章功能完成, 并完成了文章详情路由跳转

--- router/article.js
+++ router/article.js
@@ -4,4 +4,8 @@ const router = express.Router()
 
 router.get('/article/add', controller.handleArticleAddGet);
 
+router.post('/article/add', controller.handleArticleAddPost);
+
+router.get('/article/info/:id', controller.handleArticleInfoGet);
+
 module.exports = router
