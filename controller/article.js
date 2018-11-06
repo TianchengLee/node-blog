59c79441fe299f0111d2089e9e64b6482ff4b521
TianchengLee <ltc6634284@gmail.com>
2018-11-06 16:10:01 +0800

    文章发表页面渲染,使用mditor插件完成web端Markdown文本编辑器

--- controller/article.js
+++ controller/article.js
@@ -2,7 +2,7 @@ module.exports = {
   handleArticleAddGet(req, res) {
     // 判断用户的登录状态 处理登录拦截
     if (!req.session.isLogin) return res.redirect('/')
-    res.render('article.ejs', {
+    res.render('./articles/add.ejs', {
       user: req.session.user,
       isLogin: req.session.isLogin
     })
