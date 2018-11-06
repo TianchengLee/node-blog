6cbe0e1f5f93d60b0fcf08bffc4146553cc84f7b
TianchengLee <ltc6634284@gmail.com>
2018-11-06 15:55:20 +0800

    文章发表页面的登录拦截器

--- controller/article.js
+++ controller/article.js
@@ -1,5 +1,7 @@
 module.exports = {
   handleArticleAddGet(req, res) {
+    // 判断用户的登录状态 处理登录拦截
+    if (!req.session.isLogin) return res.redirect('/')
     res.render('article.ejs', {
       user: req.session.user,
       isLogin: req.session.isLogin
