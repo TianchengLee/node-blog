8243f5b490b4d8e61d0855ca27c2444718b15dec
TianchengLee <ltc6634284@gmail.com>
2018-11-08 14:58:57 +0800

    完成分页功能

--- db/db.js
+++ db/db.js
@@ -4,7 +4,8 @@ const conn = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
   password: 'root',
-  database: 'blog'
+  database: 'blog',
+  multipleStatements: true
 })
 
 module.exports = conn
